05a33a5f2789bb82c14cbf62d9fb6b92fa0a1c86
Amrutheshwar O <amrutheshwarobalesh@gmail.com>
2021-04-10 20:31:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const qr = require('qrcode');
+const _ = require('lodash');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/plasticDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const polymerSchema = {
+    polymerName: String,
+    polymerGrade: String,
+    unitWeight: Number,
+    recyclability: Number,
+};
+
+const Polymer = mongoose.model('polymer', polymerSchema);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.route('/')
+    .get(function (req, res) {
+        res.render('home')
+    })
+    .post(function (req, res) {
+        const polymerName = _.lowerCase(req.body.polymerName).split(' ').join('');
+        const polymerGrade = _.lowerCase(req.body.polymerGrade).split(' ').join('');
+        const unitWeight = _.toNumber(req.body.unitWeight);
+        const recyclability = _.toInteger(req.body.recyclability);
+        Polymer.findOne({
+            'polymerName': polymerName,
+            'polymerGrade': polymerGrade,
+            'unitWeight': unitWeight
+        }, function (err, document) {
+            if (document == null) {
+                console.log('inside here');
+                const polymer = new Polymer({
+                    polymerName: polymerName,
+                    polymerGrade: polymerGrade,
+                    unitWeight: unitWeight,
+                    recyclability: recyclability,
+                });
+                polymer.save(function (err, document) {
+                    qr.toDataURL(document.id, function (err, src) {
+                        if (!err) {
+                            const id = document.id;
+                            res.render('generator', { src, id })
+                        }
+                    })
+                });
+            } else {
+                qr.toDataURL(document.id, function (err, src) {
+                    if (!err) {
+                        const id = document.id;
+                        res.render('generator', { src, id });
+                    }
+                });
+            }
+        })
+    });
+
+app.listen(port, function () {
+    console.log("Server has started succesfully")
+})
